895774b16938c08a6e06ef4106e37ee9fb9d7dab
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2016-05-10 01:10:09 +0200

    Update start-shared.sh

--- remote-storage/start-shared.sh
+++ remote-storage/start-shared.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 UID=9999
 GID=9999
@@ -12,11 +12,13 @@ echo -n "."
 chown sambausername:sambausername /boxes/box
 if [ $? -ne 0 ]; then
         echo "Unable to change the owner of the readable directory"
+        exit 101
 fi
 echo -n "."
 chmod 777 /boxes/box
 if [ $? -ne 0 ]; then
         echo "Unable to change the file mode of readable directory"
+        exit 102
 fi
 
 echo -n "."
@@ -26,23 +28,27 @@ echo -n "."
 chown sambausername:sambausername /boxes/.box_enc
 if [ $? -ne 0 ]; then
         echo "Unable to change the owner of the encrypted directory"
+        exit 201
 fi
 echo -n "."
 chmod 777 /boxes/.box_enc
 if [ $? -ne 0 ]; then
         echo "Unable to change the file mode of encrypted directory"
+        exit 202
 fi
 
 echo -n "."
-sshfs scpuser@distant:box /boxes/.box_enc -o uid=$UID -o gid=$GID
+sshfs scpuser@distant:box /boxes/.box_enc -o reconnect -o uid=$UID -o gid=$GID
 if [ $? -ne 0 ]; then
         echo "Unable to mount encrypted directory"
+        exit 203
 fi
 
 echo -n "."
 encfs --public "/boxes/.box_enc" "/boxes/box" -o uid=$UID -o gid=$GID
 if [ $? -ne 0 ]; then
         echo "Unable to mount readable directory"
+        exit 204
 fi
 
 echo ""
