ff8e632ae3c2956e4771a32801982daea1c8bc15
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2016-05-13 00:33:27 +0200

    Create rsync-shared.sh

---
+++ pi-example/rsync-shared.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Save distant drive to a local drive (for backup)
+# You may add 'crontab -e' task running every 6 hours for instance: 0 */6 * * * ~root/rsync-remote.sh
+# and automatically mount the drive by UUID 'vim /etc/fstab ; mount -a': UUID=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx  /backup      ext4    defaults,errors=remount-ro 0       1
+
+/etc/init.d/remote-storage status
+if [ $? -ne 0 ]; then
+        echo "Status remote-storage: DOWN"
+        exit 1
+fi
+
+ret=$(cat /proc/mounts | grep "/backup" | wc -l)
+if [ "$ret" -gt 0 ]; then
+        echo "Backup directory: OK"
+else
+        echo "Backup directory: DOWN"
+        exit 2
+fi
+
+mkdir -p /backup/data
+rsync -e ssh -zv -rtgoD --delete --log-file=/backup/rsync-from-srv.log --exclude '.ssh/*' scpuser@distant:. /backup/data/
