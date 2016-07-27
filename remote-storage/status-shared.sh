9a87a0283c8c7daf82c50e8f3b155cc74fc95608
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2016-07-28 01:38:19 +0200

    Update status-shared.sh
    Better check of current status by checking the time necessary to access the drive for a quick ls command

--- remote-storage/status-shared.sh
+++ remote-storage/status-shared.sh
@@ -8,12 +8,28 @@ else
         exit 1
 fi
 
+ret=$(timeout 60 ls -alh /boxes/.box_enc)
+if [ $? -eq 0 ]; then
+        echo "Encoded directory accessible: OK"
+else
+        echo "Encoded directory accessible: DOWN -- TIMEOUT"
+        exit 11
+fi
+
 ret=$(cat /proc/mounts | grep "/boxes/box" | wc -l)
 if [ "$ret" -gt 0 ]; then
         echo "Readable directory: OK"
 else
         echo "Readable directory: DOWN"
         exit 2
+fi
+
+ret=$(timeout 60 ls -alh /boxes/box)
+if [ $? -eq 0 ]; then
+        echo "Readable directory accessible: OK"
+else
+        echo "Readable directory accessible: DOWN -- TIMEOUT"
+        exit 12
 fi
 
 ret=$(/usr/sbin/service smbd status)
